961bb5d20f6b958f7d382b5362b39f61353cc699
Sławomir Kasprowicz <slawomir.kasprowicz@gmail.com>
2016-06-01 22:37:04 +0200

    Initial commit

---
+++ SampleApi/SampleApi/src/SampleApi/Models/ITodoRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace SampleApi.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(string key);
+        TodoItem RemoItem(string key);
+        void Update(TodoItem item);
+    }
+
+    public class TodoRepository : ITodoRepository
+    {
+        private static readonly ConcurrentDictionary<string, TodoItem> _todos =
+            new ConcurrentDictionary<string, TodoItem>();
+
+        public TodoRepository()
+        {
+            Add(new TodoItem {Name = "Item1"});
+        }
+
+        public void Add(TodoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            _todos[item.Key] = item;
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todos.Values;
+        }
+
+        public TodoItem Find(string key)
+        {
+            TodoItem item;
+            _todos.TryGetValue(key, out item);
+            return item;
+        }
+
+        public TodoItem RemoItem(string key)
+        {
+            TodoItem item;
+            _todos.TryGetValue(key, out item);
+            _todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(TodoItem item)
+        {
+            _todos[item.Key] = item;
+        }
+    }
+}
